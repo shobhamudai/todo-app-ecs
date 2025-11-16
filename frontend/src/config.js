63b85fa5d92d3438d755851837377a1c5352ee78
shobha mudai <Shobha Mudai>
2025-11-16 14:40:39 -0800

    Configuration file changed

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
     export const cognitoConfig = {
-      userPoolId: 'us-east-1_smyd4WVtb', // Replace with EcsUserPoolId from CDK output
-      userPoolClientId: '6atifian7pj3grd17c1q1eljak', // Replace with EcsUserPoolClientId from CDK output
+      userPoolId: 'us-east-1_jAu5T7ZQr', // Replace with EcsUserPoolId from CDK output
+      userPoolClientId: 'oga441bnb3c7u59f6rrb6imnu', // Replace with EcsUserPoolClientId from CDK output
       region: 'us-east-1', // Replace with your AWS region if different
     };
